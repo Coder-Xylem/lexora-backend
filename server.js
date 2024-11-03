4c2c4aa6798111063c7011cc4776d0bffae8ea3f
Coder-philosopher <tentaciouspersona@gmail.com>
2024-11-03 16:13:12 +0530

    trysec

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ 
+  origin: 'https://lexora-taupe.vercel.app', 
+  credentials: true 
+}));
 app.use(cookieParser());
 app.use(express.json());
 
